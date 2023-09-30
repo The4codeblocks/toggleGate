299ccc983522035c9cad6bda49feefa368215605
The4codeblocks <the3spaceconstants@gmail.com>
2023-09-30 21:28:35 +0200

    Beginnings
    ini di beninging

--- src/SBUtils/ExampleJavaMod.java
+++ src/SBUtils/ExampleJavaMod.java
@@ -1,4 +1,4 @@
-package example;
+package SBUtils;
 
 import arc.*;
 import arc.util.*;
@@ -9,19 +9,15 @@ import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class SBUtils extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
-
+    public SBUtils(){
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });

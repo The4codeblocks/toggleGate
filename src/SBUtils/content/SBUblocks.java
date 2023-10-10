ea22a59a0cc74b2a604dc54231207638a1db3f30
The4codeblocks <the3spaceconstants@gmail.com>
2023-10-10 17:17:25 +0200

    too many imports

--- src/SBUtils/content/SBUblocks.java
+++ src/SBUtils/content/SBUblocks.java
@@ -1,12 +1,24 @@
 package SBUtils.content;
 
+
 import arc.*;
+import arc.audio.*;
+import arc.func.*;
 import arc.graphics.*;
+import arc.graphics.g2d.*;
+import arc.graphics.g2d.TextureAtlas.*;
 import arc.math.*;
-import arc.util.*;
+import arc.math.geom.*;
+import arc.scene.ui.layout.*;
+import arc.struct.EnumSet;
 import arc.struct.*;
+import arc.util.*;
+import arc.util.pooling.*;
 
 import mindustry.*;
+import mindustry.content.*;
+import mindustry.core.*;
+import mindustry.ctype.*;
 import mindustry.entities.*;
 import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
@@ -14,19 +26,30 @@ import mindustry.entities.effect.*;
 import mindustry.entities.part.DrawPart.*;
 import mindustry.entities.part.*;
 import mindustry.entities.pattern.*;
+import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
+import mindustry.graphics.MultiPacker.*;
+import mindustry.logic.*;
 import mindustry.type.*;
 import mindustry.type.unit.*;
+import mindustry.ui.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.power.*;
 import mindustry.world.consumers.*;
 import mindustry.world.draw.*;
 import mindustry.world.meta.*;
+
+import java.lang.reflect.*;
+import java.util.*;
 
 import static mindustry.Vars.*;
 import static mindustry.type.ItemStack.*;
 
 import SBUtils.content.types.blocks.*;
 import SBUtils.content.types.blocks.distribution.*;
+import SBUtils.content.types.blocks.distribution.ToggleGate; //why
 
 public class SBUblocks {
     public static Block

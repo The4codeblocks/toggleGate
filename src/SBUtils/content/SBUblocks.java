4b3ad323e24fee53a4f486381fe7b4b564eebf23
The4codeblocks <the3spaceconstants@gmail.com>
2023-10-10 17:04:35 +0200

    HOW MANY IMPORTS DO I NEED?

--- src/SBUtils/content/SBUblocks.java
+++ src/SBUtils/content/SBUblocks.java
@@ -4,27 +4,29 @@ import arc.*;
 import arc.graphics.*;
 import arc.math.*;
 import arc.util.*;
+import arc.struct.*;
 
 import mindustry.*;
-import mindustry.content.*;
 import mindustry.entities.*;
+import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
 import mindustry.entities.part.DrawPart.*;
 import mindustry.entities.part.*;
 import mindustry.entities.pattern.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
-import mindustry.ui.*;
-import mindustry.world.*;
-import mindustry.world.blocks.defense.turrets.*;
+import mindustry.type.unit.*;
+import mindustry.world.consumers.*;
 import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 
 import static mindustry.Vars.*;
 import static mindustry.type.ItemStack.*;
 
-import SBUtils.content.types.*;
+import SBUtils.content.types.blocks.*;
+import SBUtils.content.types.blocks.distribution.*;
 
 public class SBUblocks {
     public static Block
